d1e5131816df8785eb0c799f1ae3183bc19bee60
Asif <Asif_syed@homedepot.com>
2024-08-05 15:27:06 -0400

    match-case

--- Python_course_azurro/Script/match.py
+++ Python_course_azurro/Script/match.py
@@ -11,3 +11,30 @@ elif status == 404:
     
 else: 
     print('something wrong')
+    
+# By using the match and case, you can achieve same feat with cleaner code. 
+
+match status: 
+    case 401: 
+        print("Bad request ")
+    
+    case 402: 
+        print("Forbidden")
+        
+    case 404: 
+        print(" site not found")
+    
+    case _: 
+        print("Something went wrong")
+        
+# the best case for this match case is using it for string 
+user_input = input('Command: ')
+p_command = user_input.split()
+print(p_command)
+
+match p_command:
+    case ['find' ,*images]: 
+        print(f'finding: {images}')
+        
+    case _: 
+        print("Command not recognized")
